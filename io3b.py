da2e33093989950c3240297b68da08dc7c5776aa
K.-L Sun <akaronsun@gmail.com>
2013-08-07 23:33:55 +0200

    add io3b.py as example of module

---
+++ io3b.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+
+# 讀入資料的副程式
+def read_a_file(filename):
+    col1 = []
+    col2 = []
+    with open(filename,'r') as f:
+        while True:
+            line = f.readline()
+            if line:
+                tmp1, tmp2 = line.split() # 注意：split()回傳字串而不是數值
+                col1.append(float(tmp1))
+                col2.append(float(tmp2))
+            else:
+                break
+    return col1, col2
+
+wavelength1, flux1 = read_a_file('data.dat')
+wavelength2, flux2 = read_a_file('data2.dat')
+print "wavelength1", wavelength1
+print "flux1", flux1
+
+print "wavelength2", wavelength2
+print "flux2", flux2
+
